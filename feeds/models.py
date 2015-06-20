abe9af9d76407ffe35c7a7261d7009efc1528069
Prashant Chaudhary <prashantp099@gmail.com>
2015-06-21 01:05:23 +0530

    database fields defined in models

--- feeds/models.py
+++ feeds/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.core.validators import URLValidator
+from jsonfield import JSONField
 # Create your models here.
+
+class Stuend(models.Model):
+    '''
+    It stores information about the Students of college.
+    '''
+
