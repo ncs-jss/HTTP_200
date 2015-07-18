fdc90d8f9d403c213f43e4219c020faa7bd195a4
Deshraj <deshrajdry@gmail.com>
2015-07-18 15:34:03 +0530

    changes in the token returned by JWT

---
+++ HTTP_200/config.py
@@ -0,0 +1,9 @@
+# import settings
+def jwt_response_payload_handler(token, user=None, request=None):
+    # if 
+    print "###################"
+    print token
+    return {
+        'token': token,
+        'user': UserSerializer(user).data
+    }
