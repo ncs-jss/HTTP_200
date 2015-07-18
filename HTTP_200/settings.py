67f0b29242ac0e651f621a78c437826b5c15855b
Deshraj <deshrajdry@gmail.com>
2015-07-18 12:52:37 +0530

    Finally JWT working

--- HTTP_200/settings.py
+++ HTTP_200/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
 	'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+	'django.middleware.common.CommonMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,15 +157,48 @@ AUTH_USER_MODEL = 'auth.User'
 # )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ),
+	'DEFAULT_PERMISSION_CLASSES': (
+		'rest_framework.permissions.IsAuthenticated',
+	),
+	'DEFAULT_AUTHENTICATION_CLASSES': (
+		'rest_framework.authentication.SessionAuthentication',
+		'rest_framework.authentication.BasicAuthentication',
+		'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+	),
 }
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+import datetime
+
+JWT_AUTH = {
+	'JWT_ENCODE_HANDLER':
+	'rest_framework_jwt.utils.jwt_encode_handler',
+
+	'JWT_DECODE_HANDLER':
+	'rest_framework_jwt.utils.jwt_decode_handler',
+
+	'JWT_PAYLOAD_HANDLER':
+	'rest_framework_jwt.utils.jwt_payload_handler',
+
+	'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+	'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+	'JWT_RESPONSE_PAYLOAD_HANDLER':
+	'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+	'JWT_SECRET_KEY': SECRET_KEY,
+	'JWT_ALGORITHM': 'HS256',
+	'JWT_VERIFY': True,
+	'JWT_VERIFY_EXPIRATION': True,
+	'JWT_LEEWAY': 0,
+	'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300),
+	'JWT_AUDIENCE': None,
+	'JWT_ISSUER': None,
+
+	'JWT_ALLOW_REFRESH': False,
+	'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+	'JWT_AUTH_HEADER_PREFIX': 'JWT',
+}
