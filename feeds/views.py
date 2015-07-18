8486bba276dd573b42e9f4f6fffccc61dbfb337f
Deshraj <deshrajdry@gmail.com>
2015-07-18 11:55:02 +0530

    ViewSets added replacing the existing generic views

--- feeds/views.py
+++ feeds/views.py
@@ -1,174 +1,102 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+ 
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from django.contrib.auth.models import User, Group
+from django.shortcuts import render, render_to_response
 from django.http import *
-from couchbase.bucket import Bucket
 from feeds.models import *
 from feeds.serializers import *
 from feeds.forms import *
 from rest_framework import permissions
+from rest_framework import generics
+from feeds.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# cb = Bucket('couchbase:///infodb', password="123456")
-# print cb
 
 def home(request):
-	return HttpResponse("<h1>Hi there. It is working very fine.</h1> {{ form.as_p }}")
+	return render_to_response("index.html")
 
-def login(request):
-	""" Login view """
-	if not request.user.is_authenticated():
-		if request.POST:
-			username = request.POST['username']
-			password = request.POST['password']
-			user = auth.authenticate(username=username, password=password)
-			if user is not None and user.is_active:
-				# Correct password, and the user is marked "active"
-				auth.login(request,user)
-				# Redirect to a success page.
-				return HttpResponseRedirect("/")
-			else:
-				# Show an error page
-				return HttpResponse("<h3>Incorrect password</h3>")
-		return render_to_response('login.html',context_instance=RequestContext(request))
-	else:
-		return HttpResponseRedirect("/profile")
+@api_view(('GET',))
+def api_root(request, format=None):
+	return Response({
+		'users': reverse('user-list', request=request, format=format),
+		'notices': reverse('notice-list', request=request, format=format),
+		'students': reverse('student-list', request=request, format=format),
+		'faculties': reverse('faculty-list', request=request, format=format),
 
-def changepassword(request):
-	""" user password change view """
-	if user.is_authenticated:
-		if request.POST:
-			username = user.username
-			# email = request.POST['email']
-			password = request.POST['password']
-			user = User.objects.get(username=username)
-			user.set_password(password)
-			user.save()
-	return HttpResponseRedirect("/profile")
+	})
 
 
-# Below the code is for the REST API
+class StudentViewSet(viewsets.ReadOnlyModelViewSet): # Here I've used the ReadOnlyModelViewSet class to automatically provide the default 'read-only' operations
+	"""
+	This viewset automatically provides `list`, `create`, `retrieve`,
+	`update` and `destroy` actions.
 
-# from django.contrib.auth.models import User
-from feeds.serializers import StudentSerializer
-from rest_framework import generics
-# from rest_framework.permissions import IsAdminUser
-
-class StudentList(generics.ListCreateAPIView):
-	# """
-	# List all the Students or add Students 
-	# """
+	"""
+	permission_classes = (IsAuthenticatedOrReadOnly,
+		IsOwnerOrReadOnly,)
+	authentication_classes = (JSONWebTokenAuthentication, )
 	queryset = Student.objects.all()
 	serializer_class = StudentSerializer
-	# permission_classes = (IsAdminUser,)
-	paginate_by = 100
 
-	def get_paginate_by(self):
-		"""
-		Use smaller pagination for HTML representations.
-		"""
-		if self.request.accepted_renderer.format == 'html':
-			return 20
-		return 100
 
-class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+class FacultyViewSet(viewsets.ReadOnlyModelViewSet): # Here I've used the ReadOnlyModelViewSet class to automatically provide the default 'read-only' operations
 	"""
-	Retrieve, update or delete an Student instance.
-	"""
-	queryset = Student.objects.all()
-	serializer_class = StudentSerializer
-	model = Student
+	This viewset automatically provides `list`, `create`, `retrieve`,
+	`update` and `destroy` actions.
 
-class FacultyList(generics.ListCreateAPIView):
 	"""
-	List all the faculties or add faculties 
-	"""
+	permission_classes = (IsAuthenticatedOrReadOnly,
+		IsOwnerOrReadOnly,)
+	authentication_classes = (JSONWebTokenAuthentication, )
 	queryset = Faculty.objects.all()
 	serializer_class = FacultySerializer
-	# permission_classes = (IsAdminUser,)
-	# permission_classes = (IsAdminUser,)
-	paginate_by = 100
-
-	def get_paginate_by(self):
-		"""
-		Use smaller pagination for HTML representations.
-		"""
-		if self.request.accepted_renderer.format == 'html':
-			return 20
-		return 100
-
-class FacultyDetail(generics.RetrieveUpdateDestroyAPIView):
-	"""
-	Retrieve, update or delete an Faculty instance.
-	"""
-	queryset = Faculty.objects.all()
-	serializer_class = FacultySerializer
-	model = Faculty
 
 
-class NoticeList(generics.ListCreateAPIView):
+
+class NoticeViewSet(viewsets.ModelViewSet):  # I've used the ModelViewSet class in order to get the complete set of default read and write operations
 	"""
-	List all the Notices or add Notices 
+	This viewset automatically provides `list`, `create`, `retrieve`,
+	`update` and `destroy` actions.
+
 	"""
+	permission_classes = (IsAuthenticatedOrReadOnly,
+		IsOwnerOrReadOnly,)
+	authentication_classes = (JSONWebTokenAuthentication, )
 	queryset = Notice.objects.all()
 	serializer_class = NoticeSerializer
-	# permission_classes = (IsAdminUser,)
-	# permission_classes = (permissions.IsAuthenticated,)
-	paginate_by = 100
-
-	def get_paginate_by(self):
-		"""
-		Use smaller pagination for HTML representations.
-		"""
-		if self.request.accepted_renderer.format == 'html':
-			return 20
-		return 100
 
 	def perform_create(self, serializer):
 		serializer.save(faculty_id=self.request.user)
 
-class NoticeDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
 	"""
-	Retrieve, update or delete a Notices instance.
+	This viewset automatically provides `list` and `detail` actions.
 	"""
-	queryset = Notice.objects.all()
-	serializer_class = NoticeSerializer
-	model = Notice
-	# permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-# class ScheduledNoticeList(generics.ListCreateAPIView):
-# 	"""
-# 	List all the Notices or add Notices 
-# 	"""
-# 	queryset = ScheduledNotice.objects.all()
-# 	serializer_class = ScheduledNoticeSerializer
-# 	permission_classes = (IsAdminUser,)
-# 	paginate_by = 100
-
-# 	def get_paginate_by(self):
-# 		"""
-# 		Use smaller pagination for HTML representations.
-# 		"""
-# 		if self.request.accepted_renderer.format == 'html':
-# 			return 20
-# 		return 100
-
-# class ScheduledNoticeDetail(generics.RetrieveUpdateDestroyAPIView):
-# 	"""
-# 	Retrieve, update or delete a Scheduled Notices instance.
-# 	"""
-# 	queryset = ScheduledNotices.objects.all()
-# 	serializer_class = ScheduledNoticeSerializer
-# 	model = ScheduledNotice
-
-
-from feeds.serializers import UserSerializer
-
-from django.contrib.auth.models import User
-
-
-class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
-
-class UserDetail(generics.RetrieveAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
+# class check(APIView):
+#     permission_classes = (IsAuthenticated, )
+#     authentication_classes = (JSONWebTokenAuthentication, )
+ 
+#     def get(self, request):
+#     	group = None
+#     	if User.objects.filter(groups__name='student'):
+#     		group = "student"
+#     	else:
+#     		group = "faculty"
+#         data = {
+#             'id': request.user.id,
+#             'username': request.user.username,
+#             'token': str(request.auth),
+#             'type' : group
+#         }
+#         return Response(data)
