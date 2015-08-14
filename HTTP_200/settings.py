5d799dfa89659064610ebf036d12f11b65393f9e
Deshraj <deshrajdry@gmail.com>
2015-08-15 04:13:56 +0530

    Encoding enabled in JSON rendering

--- HTTP_200/settings.py
+++ HTTP_200/settings.py
@@ -167,11 +167,15 @@ REST_FRAMEWORK = {
 		'rest_framework.authentication.BasicAuthentication',
 		'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
 	),
-	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-	'PAGE_SIZE': 15,
 	'DEFAULT_FILTER_BACKENDS': (
 		'rest_framework.filters.DjangoFilterBackend',
 	),
+	'DEFAULT_RENDERER_CLASSES': (
+		'rest_framework.renderers.JSONRenderer',
+		'rest_framework.renderers.BrowsableAPIRenderer',
+	),
+	'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+	'PAGE_SIZE': 15,
 }
 
 
