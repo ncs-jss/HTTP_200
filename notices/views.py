59ddab0f9ebfdd7ea0f1bf0998a40105533ce531
deshraj <deshrajdry@gmail.com>
2015-11-11 20:33:51 +0530

    Relevent category functionality added in viewing notices

--- notices/views.py
+++ notices/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.views import generic
 from notices.models import Notice
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -7,8 +7,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .forms import NoticeCreateForm
 from profiles.models import FacultyDetail
-
 # Create your views here.
+
 class NoticeList(LoginRequiredMixin, generic.View):
 	def get(self, request):
 		template = 'notices/list.html'
@@ -26,23 +26,39 @@ class NoticeList(LoginRequiredMixin, generic.View):
 
 		return render_to_response(template, {"notices": notices})
 
-class NoticeCreateView(CreateView):
-    model = Notice
-    form_class = NoticeCreateForm
-    success_url = '/notices'
+class NoticeDetail(LoginRequiredMixin, generic.View):
+	def get(self, request, category = None):
+		if category == "relevent":
+			try:
+				# if the logged in user is a student
+				student = StudentDetail.objects.get(user__id = self.request.user.id)
+				notices = NoticeBranchYear.objects.filter(year = student.year, branch = student.branch).values_list('notice', flat = True).order_by('-created_at')
+				relevent_notices = Notice.objects.filter()
+			except:
+				# if the logged in user is a faculty
+				faculty = FacultyDetail.objects.get(user__id = self.request.user.id)
 
-    def form_valid(self, form):
-    	faculty = FacultyDetail.objects.get(user__username = self.request.user.username)
-    	form.instance.faculty = faculty
-    	form.save()
-        return super(NoticeCreateView, self).form_valid(form)
+		return 
+
+class NoticeCreateView(CreateView):
+	model = Notice
+	form_class = NoticeCreateForm
+	success_url = '/notices'
+
+	def form_valid(self, form):
+		faculty = get_object_or_404(FacultyDetail, user__id = self.request.user.id )
+		form.instance.faculty = faculty
+		form.save()
+		return super(NoticeCreateView, self).form_valid(form)
 
 class NoticeUpdateView(LoginRequiredMixin,UpdateView):
 	model = Notice
 	success_url = reverse_lazy('server_list')
-	pass
+	
+
 
 class NoticeDeleteView(LoginRequiredMixin,DeleteView):
 	model = Notice
 	success_url = reverse_lazy('server_list')
 	pass
+
