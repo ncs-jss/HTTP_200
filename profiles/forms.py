9977537b9b91865ed5ff260bce31fd24fd5aeb1b
Shivam1904 <shvam.sriv93@gmail.com>
2015-11-30 00:05:36 +0530

    Feature added: Edit Form for user now showing User details like username, firstname, lastname and email

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,6 +3,11 @@ from .models import StudentDetail, FacultyDetail
 from django.contrib.auth.models import User
 
 
+class UserForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = ['username', 'first_name', 'last_name', 'email']
+
 class StudentForm(forms.ModelForm):
 	class Meta:
 		model = StudentDetail
