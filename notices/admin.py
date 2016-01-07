c0d6d58e338c963f32316ff66d83361f82205ddf
shriyanka <shriyanka26@gmail.com>
2016-01-07 23:37:08 +0530

    Fixing notices admin

--- notices/admin.py
+++ notices/admin.py
@@ -1,21 +1,50 @@
 from django.contrib import admin
 from notices.models import Notice, BookmarkedNotice, NoticeBranchYear
-# Register your models here.
-
+from profiles.models import FacultyDetail
 
 class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'faculty', 'category', 'created_at')
+	list_display = ('faculty', 'created_at', 'title')
+	list_display_links = ('title', 'faculty')
+	list_filter = ('faculty','category')
+	list_per_page = 15
+	#search_fields = ['title', 'faculty']
 
-admin.site.register(Notice, NoticeAdmin)
+	fieldsets = (
+		(None, {
+			'classes': ('wide', 'extrapretty'),
+			'fields': ('faculty',('subject','category'),'title','description'),
+		}),
+	)
 
 
 class NoticeBranchYearAdmin(admin.ModelAdmin):
-    list_display = ('notice', 'year', 'branch')
+	list_display = ('year', 'branch', 'notice')
+	list_filter = ('faculty','category')
+	list_display_links = ('notice',)
+	list_filter = ('year','branch',)
+	list_per_page = 20
 
-admin.site.register(NoticeBranchYear, NoticeBranchYearAdmin)
+	fieldsets = (
+		(None, {
+			'classes': ('extrapretty'),
+			'fields': (('year','branch'),'notice'),
+		}),
+	)
 
 
 class BookmarkedNoticeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'notice', 'pinned')
+	list_display = ('user', 'pinned', 'notice', )
+	#list_filter = ('notice',)
+	list_per_page = 25
 
+	fieldsets = (
+		(None, {
+			'classes': ('extrapretty'),
+			'fields': (('user','notice'),'pinned'),
+		}),
+	)
+
+
+admin.site.register(Notice, NoticeAdmin)
+admin.site.register(NoticeBranchYear, NoticeBranchYearAdmin)
 admin.site.register(BookmarkedNotice, BookmarkedNoticeAdmin)
