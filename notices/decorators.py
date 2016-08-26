c5507211eaa4dd74af2fd4394a5d531d512c6eac
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-08-26 19:14:19 +0530

    Modified Decorators

--- notices/decorators.py
+++ notices/decorators.py
@@ -16,7 +16,7 @@ def student_profile_complete(function):
             profile = StudentDetail.objects.get(user=user)
         except:
             return function(request, *args, **kwargs)
-        if user.first_name == "" or user.email == "" or profile.course is None or profile.branch is None or profile.year is None or profile.contact_no is None or profile.address is None or profile.father_name is None:
+        if user.first_name == "" or user.email == "" or profile.course is None or profile.branch is None or profile.year is None or profile.contact_no == "None" or profile.address == "None" or profile.father_name == "None":
             messages.warning(request, "Fill in details to continue")
             return HttpResponseRedirect(reverse("user-profile", kwargs={"user_id": str(request.user.username)}))
         else:
