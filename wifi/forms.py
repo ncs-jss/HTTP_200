46e5cee36f4f80b462bc4ff573d7076bf1d0254c
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-09-19 00:48:13 +0530

    flake8 fixes

--- wifi/forms.py
+++ wifi/forms.py
@@ -1,12 +1,10 @@
 from django import forms
 from .models import WifiDetail
-from django.contrib.auth.models import User
 
 
 class WifiForm(forms.ModelForm):
 
     class Meta:
         model = WifiDetail
-        fields = ['laptop_mac_address',]
-        exclude = ['user',]
-        
+        fields = ['laptop_mac_address', ]
+        exclude = ['user', ]
