ea9c256a8f17e608abd739851829351401751b29
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-10-22 01:58:52 +0530

    Final fix

--- HTTP_200/settings.py
+++ HTTP_200/settings.py
@@ -109,9 +109,9 @@ WSGI_APPLICATION = 'HTTP_200.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # DATABASES = {
