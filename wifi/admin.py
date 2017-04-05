d61d4288a8217c5549a76092570214d074a2f553
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-04-05 18:53:23 +0530

    Wifi: Adds Field in the django admin.

--- wifi/admin.py
+++ wifi/admin.py
@@ -3,8 +3,8 @@ from .models import WifiDetail
 
 
 class WifiAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'email', 'name', 'laptop_mac_address', 'branch', 'mac_registered',)
-    search_fields = ('laptop_mac_address', 'user',)
+    list_display = ('__unicode__', 'email', 'name', 'laptop_mac_address', 'branch', 'mac_registered','created', 'modified')
+    search_fields = ('laptop_mac_address', 'user__username',)
 
 
 admin.site.register(WifiDetail, WifiAdmin)
