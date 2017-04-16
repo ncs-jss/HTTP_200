ff24f65676fd384bf6adee1e47f78944ecf1f974
rishabhjain <rishabhjain2018@gmail.com>
2017-04-17 01:51:00 +0530

    Notifications: API endpoint to fetch and update the preferences of user. (#67)

--- apps/notifications/api/urls.py
+++ apps/notifications/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^set_firebase_token/$', views.set_firebase_token, name='set_firebase_token'),
+    url(r'^notification_preference/$', views.notification_preference, name='notification_preference'),
 ]
