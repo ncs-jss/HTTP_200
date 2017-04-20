732c89c199a1d0ae663b1ded4f9c7e9acdcc93fa
Ankit Jain <ankitjain28may77@gmail.com>
2017-04-21 01:51:50 +0530

    Flake8 error fix (#200)

--- wifi/admin.py
+++ wifi/admin.py
@@ -3,7 +3,8 @@ from .models import WifiDetail
 
 
 class WifiAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'email', 'name', 'laptop_mac_address', 'branch', 'mac_registered','created', 'modified')
+    list_display = ('__unicode__', 'email', 'name', 'laptop_mac_address',
+                    'branch', 'mac_registered', 'created', 'modified')
     search_fields = ('laptop_mac_address', 'user__username',)
 
 
