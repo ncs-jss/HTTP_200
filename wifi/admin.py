d653287be09932489727fbc03a25a1ff672d3068
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-08-03 15:15:45 +0530

    Add Import Export Admin to wifi form.

--- wifi/admin.py
+++ wifi/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import WifiDetail
+from import_export.admin import ImportExportModelAdmin
 
 
-class WifiAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'email', 'name', 'laptop_mac_address',
-                    'branch', 'mac_registered', 'created', 'modified')
-    search_fields = ('laptop_mac_address', 'user__username',)
+class WifiAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('__unicode__', 'email', 'name', 'old_laptop_mac_address',
+    				'new_laptop_mac_address', 'branch', 'mac_registered','created', 'modified')
+    search_fields = ('old_laptop_mac_address', 'new_laptop_mac_address', 'user__username',)
 
 
 admin.site.register(WifiDetail, WifiAdmin)
