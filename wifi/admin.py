2c04da7e3926354958e8ab4e572d9158d354ca4e
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-08-05 05:23:17 +0530

    Add new laptop field in the wifi xls file.

--- wifi/admin.py
+++ wifi/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import WifiDetail
-from import_export.admin import ImportExportModelAdmin
 
 
-class WifiAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+class WifiAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'email', 'name', 'old_laptop_mac_address',
     				'new_laptop_mac_address', 'branch', 'mac_registered','created', 'modified')
     search_fields = ('old_laptop_mac_address', 'new_laptop_mac_address', 'user__username',)
--- wifi/views.py
+++ wifi/views.py
@@ -81,51 +81,54 @@ class FacultyWifiForm(LoginRequiredMixin, View):
                 return HttpResponseRedirect(reverse("faculty-wifi"))
 
 
-# class excel_writer(LoginRequiredMixin, View):
+class excel_writer(LoginRequiredMixin, View):
 
-#     def get(self, request):
-#         '''
-#         Custom class to download Xls File .
-#         '''
+    def get(self, request):
+        '''
+        Custom class to download Xls File .
+        '''
 
-#         workbook = xlsxwriter.Workbook("wifi_details.xls")
-#         worksheet = workbook.add_worksheet()
-#         wifi = WifiDetail.objects.all()
-#         bold = workbook.add_format({'bold': True})
-#         worksheet.set_column(1, 160, 7)
-#         columns = ["Username", "First Name", "Last Name", "Course", "Branch", "Year", "Laptop Mac Address", "Date Applied"]
-#         row = 0
-#         for i, elem in enumerate(columns):
-#             worksheet.write(row, i, elem, bold)
+        workbook = xlsxwriter.Workbook("wifi_details.xls")
+        worksheet = workbook.add_worksheet()
+        wifi = WifiDetail.objects.all()
+        bold = workbook.add_format({'bold': True})
+        worksheet.set_column(1, 160, 7)
+        columns = ["Username", "First Name", "Last Name", "Course", "Branch", "Year", "Old_Laptop Mac Address", "New_Laptop_mac_address", "Date Applied"]
+        row = 0
+        for i, elem in enumerate(columns):
+            worksheet.write(row, i, elem, bold)
 
-#         row += 1
-#         for users in wifi:
-#             date_registered = str(users.created).split(' ')[0]
-#             try:
-#                 user = User.objects.get(username=users.user)
-#                 student = StudentDetail.objects.get(user=user)
-#                 worksheet.write(row, 0, user.username)
-#                 worksheet.write(row, 1, user.first_name)
-#                 worksheet.write(row, 2, user.last_name)
-#                 worksheet.write(row, 3, student.course)
-#                 worksheet.write(row, 4, student.branch)
-#                 worksheet.write(row, 5, student.year)
-#                 worksheet.write(row, 6, users.laptop_mac_address)
-#                 worksheet.write(row, 7, date_registered)
-#                 row += 1
-#             except:
-#                 user = User.objects.get(username=users.user)
-#                 faculty = FacultyDetail.objects.get(user=user)
-#                 worksheet.write(row, 0, user.username)
-#                 worksheet.write(row, 1, user.first_name)
-#                 worksheet.write(row, 2, user.last_name)
-#                 worksheet.write(row, 4, faculty.department)
-#                 worksheet.write(row, 6, users.laptop_mac_address)
-#                 worksheet.write(row, 7, date_registered)
-#                 row += 1
+        row += 1
+        for users in wifi:
+            print users
+            date_registered = str(users.created).split(' ')[0]
+            try:
+                user = User.objects.get(username=users.user)
+                student = StudentDetail.objects.get(user=user)
+                worksheet.write(row, 0, user.username)
+                worksheet.write(row, 1, user.first_name)
+                worksheet.write(row, 2, user.last_name)
+                worksheet.write(row, 3, student.course)
+                worksheet.write(row, 4, student.branch)
+                worksheet.write(row, 5, student.year)
+                worksheet.write(row, 6, users.old_laptop_mac_address)
+                worksheet.write(row, 7, users.new_laptop_mac_address)
+                worksheet.write(row, 8, date_registered)
+                row += 1
+            except:
+                user = User.objects.get(username=users.user)
+                faculty = FacultyDetail.objects.get(user=user)
+                worksheet.write(row, 0, user.username)
+                worksheet.write(row, 1, user.first_name)
+                worksheet.write(row, 2, user.last_name)
+                worksheet.write(row, 4, faculty.department)
+                worksheet.write(row, 6, users.old_laptop_mac_address)
+                worksheet.write(row, 7, users.new_laptop_mac_address)
+                worksheet.write(row, 8, date_registered)
+                row += 1
 
-#         workbook.close()
-#         response = HttpResponse(file("wifi_details.xls"))
-#         response['Content-Type'] = "application/vnd.ms-excel"
-#         response['Content-Disposition'] = 'attachment; filename="wifi_details.xls"'
-#         return response
+        workbook.close()
+        response = HttpResponse(file("wifi_details.xls"))
+        response['Content-Type'] = "application/vnd.ms-excel"
+        response['Content-Disposition'] = 'attachment; filename="wifi_details.xls"'
+        return response
