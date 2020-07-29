fe97a7fe6bd0ef615b4b278e78a40fccd7d17b54
singhsourabh <singhsourabh14@gmail.com>
2020-07-29 16:19:14 +0530

    Enabled haystack

--- HTTP_200/settings.py
+++ HTTP_200/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    # 'haystack',
+    'haystack',
     # 'feeds',
     'allauth',
     'allauth.account',
